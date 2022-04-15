69bce4539b369a3cafbc97e4eb0b6930b199b379
dlucenad <dlucenad.daniel@gmail.com>
2022-04-15 11:16:22 -0300

    adding F5 on NAV and - tap on LCTL

--- bm40/dlucenad_abnt2/keymap.c
+++ bm40/dlucenad_abnt2/keymap.c
@@ -43,16 +43,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //qwerty base layer ok
 [_QWERTY] = LAYOUT_planck_mit(
-     KC_GRV,    KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,    KC_Y,  KC_U,            KC_I,   KC_O,    KC_P, BR_ACUT,
-      MYTAB,    KC_A,    KC_S,  KC_D,  KC_F,  KC_G,    KC_H,  KC_J,            KC_K,   KC_L, BR_CCED, BR_TILD,
-    KC_LSPO,    KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,         KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
-    KC_LCTL, KC_LGUI, KC_LALT,   SYM, MYNAV,      KC_SPC,    MYNUM, RALT_T(KC_LEFT),    FUN,   KC_UP, RCTL_T(KC_RGHT)
+             KC_GRV,    KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,    KC_Y,  KC_U,            KC_I,   KC_O,    KC_P, BR_ACUT,
+              MYTAB,    KC_A,    KC_S,  KC_D,  KC_F,  KC_G,    KC_H,  KC_J,            KC_K,   KC_L, BR_CCED, BR_TILD,
+            KC_LSPO,    KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,         KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
+    LCTL_T(KC_MINS), KC_LGUI, KC_LALT,   SYM, MYNAV,      KC_SPC,    MYNUM, RALT_T(KC_LEFT),    FUN,   KC_UP, RCTL_T(KC_RGHT)
 ),
 
 //navigation and utility layer ok
 [_NAV] = LAYOUT_planck_mit(
      KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP, KC_VOLU,
-    _______, _______, _______, _______, _______, _______,  KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_VOLD,
+    _______,   KC_F5, _______, _______, _______, _______,  KC_ENT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_VOLD,
     _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, KC_BSPC,  KC_DEL, _______, _______, KC_MUTE,
     _______, _______, _______, _______, _______,     _______,        MYNUM, _______, KC_MPRV, KC_MPLY, KC_MNXT
 ),
